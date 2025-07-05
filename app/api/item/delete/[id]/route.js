84b8921105621b42bdc1f274961b19a89251c719
sjkim-pro <lunaksj@naver.com>
2025-07-05 18:09:23 +0900

    처음부터 시작하는 Next.js React 개발 입문 Chapter 3 아이템 삭제 까지 완료

---
+++ app/api/item/delete/[id]/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import connectDB from "../../../../utils/database";
+import { ItemModel } from "../../../../utils/schemaModels";
+
+export async function DELETE(request, context) {
+    try {
+        const params = await context.params;
+        await connectDB();
+        await ItemModel.deleteOne({_id: params.id});
+        return NextResponse.json({message:"아이템 삭제 성공"});
+    } catch (e) {
+        return NextResponse.json({message:"아이템 삭제 실패"});
+    }
+}
