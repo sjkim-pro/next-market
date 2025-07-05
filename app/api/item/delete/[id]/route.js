5cbdd8f33d521bb973bed0d6eb859f126a2b7c8f
sjkim-pro <lunaksj@naver.com>
2025-07-05 20:50:19 +0900

    처음부터 시작하는 Next.js React 개발 입문 Chapter 4-1, 4-2 까지 완료

--- app/api/item/delete/[id]/route.js
+++ app/api/item/delete/[id]/route.js
@@ -8,7 +8,7 @@ export async function DELETE(request, context) {
         await connectDB();
         await ItemModel.deleteOne({_id: params.id});
         return NextResponse.json({message:"아이템 삭제 성공"});
-    } catch (e) {
+    } catch {
         return NextResponse.json({message:"아이템 삭제 실패"});
     }
 }
